2a22e00cd5934f6af34ae8976bae0541aec0118a
ErisedKT <sanyaverma0303@gmail.com>
2019-07-14 08:04:52 +0530

    fix timestamp

--- search_engine/search_engine/__init__.py
+++ search_engine/search_engine/__init__.py
@@ -1,13 +1,14 @@
 #! /usr/bin/python
 # __init__.py - Make a basic crawler.
 
-import requests, time
+import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 def crawler(url):
     res = requests.get(url)
     res.raise_for_status()
-    access_time = time.time()
+    access_time = datetime.strftime(datetime.now(), '%H:%M:%S')
     soup = BeautifulSoup(res.text)
     links = soup.select('a')
     return res.text, access_time, links
