b9d5c134f604586bc8dc0f8e2e1581d0e164acea
Vansh Sood <emailofvansh@gmail.com>
2019-07-14 19:11:21 +0530

    Themes: Dark and light

--- assets/func.js
+++ assets/func.js
@@ -19,9 +19,29 @@ $('.b2').click(function(){
     var x = $('.searchbar').val();
       if( x.length === 0){
       $('.whyno').addClass('show1');
-
     }
     $('.searchbar2').val(x);
 })
+$('.it2').click(function(){
+
+  var css = 'html {-webkit-filter: invert(100%);' + '-moz-filter: invert(100%);' + '-o-filter: invert(100%);' + '-ms-filter: invert(100%); }';
+  var head = document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  if (!window.counter) {
+      window.counter = 1;
+    } else {
+      window.counter++;
+      if (window.counter % 2 == 0) {
+          var css = 'html {-webkit-filter: invert(0%);}'
+        }
+      }
+      style.type = 'text/css';
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      head.appendChild(style);
+    })
 
 })
