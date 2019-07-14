1b47284a5107f143de644873825ceb93a488e0a4
ErisedKT <sanyaverma0303@gmail.com>
2019-07-14 15:53:58 +0530

    Create parser

---
+++ search_engine/search_engine/parser.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+# parser.py - Reads the data fetched by the crawler, parses it, returns whatever metadata it needs to.
+
+from html.parser import HTMLParser
+import requests
+from datetime import datetime
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            attr = dict(attrs)
+            if 'href' in attr:
+                self.links.append(attr)
+
+def crawler(url):
+    # Download webpage.
+    res = requests.get(url)
+    res.raise_for_status()
+
+    # Format time of call as HH:MM:SS:MS.
+    access_time = datetime.strftime(datetime.now(), '%H:%M:%S:%f')
+
+    # Create parser object to search for links.
+    parser = MyHTMLParser()
+    parser.links = []
+    parser.feed(res.text)   # feed source code to parser
+    links = [link['href'] for link in parser.links]
+    return res.text, access_time, links
+
+
+class metaParser(HTMLParser):
+    metadata = []
+    def handle_starttag(self, tag, attrs):
+        if tag == 'meta' or tag == 'title' or tag == 'link':
+            attr = dict(attrs)
+            
+
+
+def parser(source_code):
+    myParser = metaParser()
+    myParser.metadata = []
+    myParser.feed(source_code)
+    for m in myParser.metadata:
+        print(m)
+    return myParser.metadata
+
+parser(crawler('https://pypi.org/project/metadata-parser/')[0])
