6c667537f15dc4eb338b665e3169a2e9a2dae156
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-07-15 21:04:34 +0530

    pep fix

--- search_engine/search_engine/crawler.py
+++ search_engine/search_engine/crawler.py
@@ -2,10 +2,10 @@ import requests
 import re
 import datetime
 
+
 def crawl_web(initial_url):
     crawl = []
     crawl.append(initial_url)
-
     while crawl:
         current_url = crawl.pop(0)
         r = requests.get(current_url)
@@ -18,5 +18,4 @@ def crawl_web(initial_url):
         title = re.findall(r'<title>(.*)</title>', str(r.content))
         date = datetime.datetime.now()
         return crawl, title, date
-
 print(crawl_web("https://xino.in/"))
