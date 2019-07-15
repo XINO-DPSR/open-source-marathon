bfadf7e050e04e44fa33f204c118618ded0cac43
Laxya Pahuja <laxya.pahuja8@gmail.com>
2019-07-16 00:37:45 +0530

    fix v2

--- search_engine/search_engine/crawler.py
+++ search_engine/search_engine/crawler.py
@@ -9,7 +9,7 @@ def crawl_web(initial_url):
     while crawl:
         current_url = crawl.pop(0)
         r = requests.get(current_url)
-        for url in re.findall('<a href="([^"]+)">', str(r.content)):
+        for url in re.findall('href="([^"]+)"', str(r.content)):
             if url[0] == '/':
                 url = current_url + url[1:]
             pattern = re.compile('https?')
@@ -17,5 +17,6 @@ def crawl_web(initial_url):
                 crawl.append(url)
         title = re.findall(r'<title>(.*)</title>', str(r.content))
         date = datetime.datetime.now()
-        return crawl, title, date, str(r.content)
-print(crawl_web("https://xino.in/"))
+        src = str(r.text)
+        return crawl, title, date, src
+print(crawl_web("https://sayamkanwar.com/"))
