badd1ee09cdc24b6f5d80b09571b56b0b48e846d
mahansh564 <mah.ansh564@gmail.com>
2019-07-16 23:31:28 +0530

    PEP 8

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import urllib2, sys 
-import HTMLParser 
+import urllib2
+import sys
+import HTMLParser
+
 
 class Parser(HTMLParser.HTMLParser):
 
-   anchors = list()
+    anchors = list()
 
-   #HTML Parser Methods
-   def handle_starttag(self, startTag, attrs):
-       if startTag == "a":
-           for attr in attrs:
-               if attr[0] == "href":
-                   self.anchors.append(attr[1])
+    # HTML Parser Methods
+    def handle_starttag(self, startTag, attrs):
+        if startTag == "a":
+            for attr in attrs:
+                if attr[0] == "href":
+                    self.anchors.append(attr[1])
 
-def main(url): 
-    headers = {} 
-    headers["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0" 
-    req = urllib2.Request(url, headers = headers)
+
+def main(url):
+    headers = {}
+    headers["User-Agent"] = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
+    req = urllib2.Request(url, headers=headers)
     res = urllib2.urlopen(req)
     info = res.info()
     parser = Parser()
@@ -28,6 +31,7 @@ def main(url):
     for i in parser.anchors:
         print i
 
-if __name__ == '__main__':
-    main(sys.argv[1]) # Passes in URL as parameter ex. (python main.py https://google.com)
 
+if __name__ == '__main__':
+    # Passes in URL as parameter ex. (python main.py https://google.com)
+    main(sys.argv[1])
