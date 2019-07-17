88f45bccfba7da60dfcd97ec2aeadb52f42c8dc2
Pranav Gade <pranavgade20@gmail.com>
2019-07-17 18:43:50 +0530

    fixed all issues.

--- search_engine/search_engine/webcrawler.py
+++ search_engine/search_engine/webcrawler.py
@@ -6,15 +6,12 @@ import requests
 def crawl(url):
     response = requests.get(url)
     page = response.text
-    baseUrl = response.url
-    matches = re.findall("<a((?!href).)* href=\"(?!\")([-a-zA-Z0-9@:%_\+.~#?&//=\s]*)\">", page)
+    baseurl = response.url
+    matches = re.findall('<a((?!href).)* href=\"(?!\")([-a-zA-Z0-9@:%_\+.~#?&//=\s]*)\">', page)
     urls = []
     for match in matches:
-        links = re.findall("href=\"(?!\")([-a-zA-Z0-9@:%_\+.~#?&//=\s]*)\">", match[1])
-        for link in links:
-            sub = link[6, -1]
-            if sub.startswith('/'):
-                urls.append(baseUrl+sub)
-            else:
-                urls.append(sub)
+        if match[1].startswith('/'):
+            urls.append(baseurl+match[1])
+        else:
+            urls.append(match[1])
     return page, datetime.datetime.utcnow().timestamp(), urls
