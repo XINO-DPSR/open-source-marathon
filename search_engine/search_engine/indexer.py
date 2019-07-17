72277e3101c5f22b312564526093492220a56d23
ErisedKT <sanyaverma0303@gmail.com>
2019-07-18 01:19:41 +0530

    Add indexer function

--- search_engine/search_engine/indexer.py
+++ search_engine/search_engine/indexer.py
@@ -1,5 +1,22 @@
 #! /usr/bin/python
 # indexer.py - Make a basic indexer and database structure.
 
-def indexer(source_code, content):
-    inverted_index = [[], []]
+import json
+import parser
+import requests
+
+# Test code
+# res = requests.get('https://sayamkanwar.com')
+# metadata, words, source_code = parser.parser(res.text)
+
+def indexer(metadata, words, source_code):
+    inverted_index =  {}
+    for i in range(len(words)):
+        inverted_index[words[i]] = [i]
+        inverted_index[words[i]].append(source_code.index(words[i]))
+    db = open('jsonDB.json', 'w')
+    jsonString = json.dumps(inverted_index)
+    db.write(jsonString)
+    db.close()
+
+# indexer(metadata, list(words), source_code)
---
+++ search_engine/search_engine/parser.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+# parser.py - Reads the data fetched by the crawler, parses it,
+# returns whatever metadata it needs to.
+
+from html.parser import HTMLParser
+
+
+class metaParser(HTMLParser):
+
+    # Stores metadata in list of dictionaries.
+    def handle_starttag(self, tag, attrs):
+        if tag == 'meta' or tag == 'link' or tag == 'script' or \
+                tag == 'title' or tag == 'style':
+            attr = dict(attrs)
+            attr['tag'] = tag
+            self.metadata.append(attr)
+
+    def handle_data(self, data):
+        words = data.split(' ')
+        for word in words:
+            ascii_word = ''.join(
+                [l if ord(l) < 128 and ord(l) > 32 else ' '
+                    for l in word])
+            ascii_word = ascii_word.split(' ')
+            for w in ascii_word:
+                if w != '':
+                    self.source_text.append(w)
+
+
+def parser(source_code):
+    '''
+    Input: Source code obtained as first return \
+        value of crawler method.
+    Returns: myParser.metadata - List of dictionaries \
+                containing metadata information.
+            data - Set of all individual words found \
+                in source code.
+    '''
+    myParser = metaParser()
+    myParser.metadata = []
+    myParser.source_text = []
+    myParser.feed(source_code)
+    words = set(myParser.source_text)
+    return myParser.metadata, words, source_code
