7bae3986a69c40a3177e3d9028b8fbc9c6ee143e
ErisedKT <sanyaverma0303@gmail.com>
2019-07-18 15:35:01 +0530

    Add crawler unit tests

--- search_engine/search_engine/test_crawler.py
+++ search_engine/search_engine/test_crawler.py
@@ -1,8 +1,38 @@
 import unittest
-import ff_crawler
-import webcrawler
+from ff_crawler import ff_crawler
+from ff_crawler import ff_crawler_driver
+import requests
 
-class TestCrawler(unittest.TestCase):
 
-    def test_crawler(self):
-        self.
+class TestParser(unittest.TestCase):
+
+    
+    def test_ff_crawler(self):
+
+        # When the domain disallows crawling, the crawler should
+        # return an empty list as its third return value.
+
+        self.assertListEqual(ff_crawler('http://coreis.us')[2], [])
+        
+        # No links should start with mailto: or tel:
+
+        flag = False
+        for link in ff_crawler('https://www.scottseverance.us/mailto.html')[2]:
+            if link.startswith('mailto:') or link.startswith('tel:'):
+                flag = True
+                break
+
+        self.assertEqual(flag, False)
+        
+    def test_ff_crawler_driver(self):
+
+        # When depth is set to 1 for example.com, should return
+        # dictionary with two levels.
+
+        self.assertDictEqual(ff_crawler_driver('https://example.com', 1), 
+            {0: ['https://example.com'], 1: ['http://www.iana.org/domains/example']})
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
