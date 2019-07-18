8b100cef395729bd365144b2ad8fa55410aff0ed
Vansh Sood <emailofvansh@gmail.com>
2019-07-18 16:59:57 +0530

    indexer

--- main.py
+++ main.py
@@ -1,67 +1,60 @@
+import requests
+import sys
 import time
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-from bs4.element import Comment
+from html.parser import HTMLParser
+from datetime import datetime
+from os import path
+from urllib import robotparser
+from urllib.parse import urljoin
+class MyHTMLParser(HTMLParser):
+    def handle_startpageTag(self, pageTag, attributes):
+        if pageTag == 'a':
+            attribute = dict(attributes)
+            if 'href' in attributeibute and not attribute['href'].startswith('mailto:') \
+                    and not attribute['href'].startswith('tel:'):
+                self.links.append(attribute)
 
+def effCrawlerDriver(link, dep=5):
+    remainingLinks = effCrawler(link)[2]
+    all_urls = {0: [link], 1: remainingLinks}
+    done = [link]
+    layer = 1
+    while layer < dep:
+        all_urls[layer + 1] = []
+        for link in all_urls[layer]:
+            if link not in done:
+                try:
+                    all_urls[layer + 1] += effCrawler(link)[2]
+                    done.append(link)
+                except:
+                    continue
+        layer += 1
+    return all_urls
 
-def tag_visible(element):
-    if element.parent.name in [
-        'style',
-        'script',
-        'head',
-        'title',
-        'meta',
-            '[document]']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
+def effCrawler(link):
+    robot_parser = robotparser.RobotFileParser()
+    robot_parser.set_url(path.join(link, 'robots.txt'))
+    wait = 0
+    if not robot_parser.read() is None:
+        if not robot_parser.can_fetch('*', link):
+            return '', None, [], 0
+        if not robot_parser.crawl_delay('*') is None:
+            wait = robot_parser.crawl_delay('*')
 
+    # Request webpage
+    res = requests.get(link)
+    res.raise_for_status()
+    # Formatting Date Time to Month/Date/Year, Hour:Minutes:Second format
+    access_time = time.strftime("%m/%d/%Y, %H:%M:%S")
+    # Creating parser instance
+    parser = MyHTMLParser()
+    parser.links = []
+    parser.feed(res.text)
+    links = [urljoin(link, i['href']) for i in parser.links]
+    return res.text, access_time, links, wait
 
-def crawl(link):
-    # Open the url
-    src = urlopen(link)
-    # Parse it in beautifulsoup
-    codebase = BeautifulSoup(src, 'html.parser')
-    prettyCode = codebase.prettify()
-    # Finding all text elements in the webpage
-    texts = codebase.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)
-    # Filtering text to clean all the text in the webpage
-    text = u" ".join(t.strip() for t in visible_texts)
-    # Fetching all the anchor tags
-    listOfAnchor = codebase.findAll("a")
-    urls = []
-    urlText = []
-    for i in listOfAnchor:
-        # Get text of anchor tag
-        a = i.getText()
-        # Get URL of anchor tag
-        b = i.get("href")
-        # maintaining a list of the text of url's
-        urlText.append(a)
-        # maintaining a list of URL's
-        urls.append(b)
-    print("Timestamp: " + time.strftime("%m/%d/%Y, %H:%M:%S"))
-    # Looping to print text of URL's and URL's
-    for i in range(len(urlText)):
-        print("URL text =>> " + urlText[i])
-        print("\n\n")
-        print("Link = > " + urls[i])
-    input("Press enter to crawl...")
-    # Printing pretty soup
-    print("Source Code of the webpage => ")
-    print(prettyCode)
-    input("Press enter to parse...")
-    # Printing visible text of the webpage
-    print("Text of the webpage => ")
-    print(text)
-    # Printing metadata
-    print("Metadata of the webpage => ")
-    print(urls)
-
-# Take a URL input
-x = input(
-    "Enter any url (PS, copy the url directly from the web browser for easy usage) : ")
-# Call the crawler
-crawl(x)
+x = input("Enter webpage to be crawled: ")
+print("Starting crawling...")
+time.sleep(1)
+result = effCrawler(x)
+print(result)
